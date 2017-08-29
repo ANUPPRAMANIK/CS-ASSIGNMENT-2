3aab90e82bc3def2715658793d20fd908903ea28
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-29 16:32:48 +0530

    Create Lab2_question8.cpp

---
+++ Lab2_question8.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+int a,b,c;
+cout<< " enter the base and height of your triangle ";
+cin>>a>>b;
+cout<<" the base and height of your triangle are: "<<a<<endl<<b;
+c=(a*b)/2;
+cout<<" the area of your triangle is: "<<c;
+}
