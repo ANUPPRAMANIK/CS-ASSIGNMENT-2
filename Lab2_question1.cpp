35727152832ad83fbc7c47292b26ae086ea65de5
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-29 16:09:20 +0530

    Create Lab2_question1.cpp

---
+++ Lab2_question1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std; 
+main()
+{
+float a,b,c;
+cout<< " enter your length in centimeters ";
+cin>>a;
+cout<< " your length in centimeters is: "<<a;
+b=a/100;
+cout<< " your length in meters is: "<<b;
+c=a/1000;
+cout<< " your length in kilometers is: "<<c;
+}
