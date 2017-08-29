badd831633e131d96258e1be8814aac06af3cd1a
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-29 16:14:30 +0530

    Create Lab2_question2.cpp

---
+++ Lab2_question2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main ()
+{
+float a,b,f;
+cout<< " enter your temperature in celcius ";
+cin>>a;
+cout<< " your temperature in celcius is: "<<a;
+b=(9*a)/5+32;
+cout<< " your temperature in fahrenheit is: "<<b;
+}
