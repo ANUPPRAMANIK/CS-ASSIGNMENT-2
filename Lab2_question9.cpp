190d5095bbe5878c21be13763ea49db30ceb662f
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-29 16:43:35 +0530

    Create Lab2_question9.cpp

---
+++ Lab2_question9.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main(){
+float a,b,c,d,e;
+cout<<" enter the length of side of the equilateral triangle ";
+cin>>a;
+b=(1.732/4)*(a*a);
+cout<<" the area of your equilateral triangle is: "<<b;
+}
