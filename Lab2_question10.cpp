f717cda6553408e5f39b86b3d61c1f2645fe4a2b
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-29 16:54:22 +0530

    Create Lab2_question10.cpp

---
+++ Lab2_question10.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+float a,b,c,d,e,f,g,h,i,j;
+cout<<" enter marks of 5 subjects(out of hundred) ";
+cin>>a>>b>>c>>d>>e;
+f=a+b+c+d+e;
+cout<<" total: "<<f;
+g=(a+b+c+d+e)/5;
+cout<<" average: "<<g;
+h=(f/500)*100;
+cout<<" percentage: "<<h;
+}
