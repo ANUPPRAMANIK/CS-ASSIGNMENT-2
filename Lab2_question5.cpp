21deff1f7410222dc760a3c0b12e328aaccda6f7
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-29 16:16:57 +0530

    Create Lab2_question5.cpp

---
+++ Lab2_question5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main(){
+int a,b,c,d,e;
+cout<<" enter your base number ";
+cin>>a;
+cout<< " your base number is: "<<a;
+cout<<" enter your exponent ";
+cin>>b;
+cout<<" your exponent is: "<<b;
+c=pow(a,b);
+cout<<" answer: "<<c;
+}
