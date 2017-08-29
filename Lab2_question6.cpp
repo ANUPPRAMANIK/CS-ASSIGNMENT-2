ec78844880d718105c2d0122ded4284e50a06752
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-29 16:18:09 +0530

    Create Lab2_question6.cpp

---
+++ Lab2_question6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main (){
+float a,b,c,d,e;
+cout<< " enter your number ";
+cin>>a;
+cout<< " your number is: "<<a;
+b=sqrt(a);
+cout<< " the square root of your number is: "<<b;
+}
