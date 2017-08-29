334aa4f7f42e474f68b347d764b4e19ee7e1c3db
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-29 17:41:04 +0530

    Update Lab2_question12.cpp

--- Lab2_question12.cpp
+++ Lab2_question12.cpp
@@ -1,1 +1,10 @@
-
+#include<iostream>
+#include<cmath>
+using namespace std;
+main(){
+float a,b,c,d,e;
+cout<<" enter your principle(P)(in rupees), rate(R)(in %), time(T)(in years) and the number of times the interest is compounded per year(N) ";
+cin>>a>>b>>c>>d;
+e=a*pow(1+b/100*1/d,c*d)-a;
+cout<< " compound interest: "<<e;
+}
