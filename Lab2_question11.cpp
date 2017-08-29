d5f54484d5867a2f24ce04c42e0e489570af8584
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-29 17:06:53 +0530

    Create Lab2_question11.cpp

---
+++ Lab2_question11.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main(){
+float a,b,c,d;
+cout<< " enter your principle(P)(in rupees), time(T)(in years) and rate(R)(in %) ";
+cin>>a>>b>>c;
+d=(a*b*c)/100;
+cout<< " simple interest: "<<d;
+}
